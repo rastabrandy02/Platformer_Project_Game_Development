13ab77aec47a7b20bf98e377c47d169e0991c336
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2021-10-11 13:16:29 +0200

    Created the template and basic movement

---
+++ Platformer_Project/ModulePlayer.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "p2Point.h"
+
+class PhysBody;
+
+struct Object
+{
+	SDL_Texture* graphic;
+	PhysBody* body;
+	uint fx;
+
+	Object() : graphic(NULL), body(NULL)
+	{}
+};
+
+class ModulePlayer : public Module
+{
+public:
+	ModulePlayer(Application* app, bool start_enabled = true);
+	virtual ~ModulePlayer();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status Update();
+	update_status PostUpdate();
+	bool CleanUp();
+
+public:
+
+	Object ball;
+	Object flipper1;
+	Object flipper2;
+	Object spring;
+
+	PhysBody* flipper1_wheel;
+	PhysBody* flipper2_wheel;
+	PhysBody* spring_wheel;
+
+	Object flipper_up1;
+	Object flipper_up2;
+	PhysBody* flipper_up1_wheel;
+	PhysBody* flipper_up2_wheel;
+
+	PhysBody* sensor1;
+
+	p2Point <int> position;
+	
+	SDL_Rect playerRec;
+	int speedX = 10;
+	int speedY = 20;
+	bool isFalling;
+	bool isJumping = false;
+	int jumpStartPos;
+	int maxJump = 150;
+	
+};
